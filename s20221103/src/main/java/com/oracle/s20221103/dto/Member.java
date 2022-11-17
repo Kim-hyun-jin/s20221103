a5d214e839c8a3d9a864c2f709b69c4c74ed4622
PEJ <PEJ@DESKTOP-7OECC0U>
2022-11-17 16:58:51 +0900

    member dto

--- s20221103/src/main/java/com/oracle/s20221103/dto/Member.java
+++ s20221103/src/main/java/com/oracle/s20221103/dto/Member.java
@@ -1,5 +1,15 @@
 package com.oracle.s20221103.dto;
 
 public class Member {
-
+	private String 	id;				// 회원번호
+	private String 	username;		// 회원아이디
+	private String 	role;			// 권한
+	private String 	password;		// 비밀번호
+	private Long	memberCall;		// 연락처
+	private String	memberName;		// 이름
+	private String	memberAddress;	// 주소
+	private String	memberEmail;	// 이메일
+	private String	profImg;		// 프로필사진
+	private String	teacher;		// 담당선생님
+	private Long	memberDrop;		// 탈퇴여부
 }
---
+++ s20221103/src/main/java/com/oracle/s20221103/pej/repository/PejRepository.java
@@ -0,0 +1,5 @@
+package com.oracle.s20221103.pej.repository;
+
+public interface PejRepository {
+
+}
